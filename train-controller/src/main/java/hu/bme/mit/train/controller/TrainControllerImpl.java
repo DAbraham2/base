c93fddf127823e5dafda49790cd58e499d2bc072
Daniel Abraham <daniel.abraham@edu.bme.hu>
2021-05-19 03:14:01 +0200

    guava

--- train-controller/src/main/java/hu/bme/mit/train/controller/TrainControllerImpl.java
+++ train-controller/src/main/java/hu/bme/mit/train/controller/TrainControllerImpl.java
@@ -1,6 +1,10 @@
 package hu.bme.mit.train.controller;
 
+import com.google.common.collect.HashBasedTable;
+import com.google.common.collect.Table;
 import hu.bme.mit.train.interfaces.TrainController;
+
+import java.util.Date;
 import java.util.Timer;
 import java.util.TimerTask;
 
@@ -9,6 +13,8 @@ public class TrainControllerImpl implements TrainController {
 	private int step = 0;
 	private int referenceSpeed = 0;
 	private int speedLimit = 0;
+
+	private Tachograph tacho = new Tachograph();
 
 	public TrainControllerImpl() {
 		TimerTask timerTask = new TimerTask() {
@@ -35,6 +41,8 @@ public class TrainControllerImpl implements TrainController {
 		}
 
 		enforceSpeedLimit();
+
+		tacho.record(this.step, this.referenceSpeed);
 	}
 
 	@Override
@@ -58,5 +66,18 @@ public class TrainControllerImpl implements TrainController {
 	@Override
 	public void setJoystickPosition(int joystickPosition) {
 		this.step = joystickPosition;
+	}
+
+
+	class Tachograph {
+		private Table<Long, Integer, Integer> records = HashBasedTable.create();
+
+		public void record(int joystick, int refSpeed){
+			long now = System.currentTimeMillis();
+
+			System.out.println(String.format("Record made: %d - %d - %d", now, joystick, refSpeed));
+
+			records.put(now, joystick, refSpeed);
+		}
 	}
 }
